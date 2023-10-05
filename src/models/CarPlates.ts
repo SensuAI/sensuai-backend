830168d7326f92e3d718ff08049722aac0dd725b
Eduardo Cortez Valente <130951926+EduardoCorVal@users.noreply.github.com>
2023-10-04 18:02:56 -0600

    feature(CarPlates): Created

--- src/models/CarPlates.ts
+++ src/models/CarPlates.ts
@@ -3,3 +3,9 @@ import { Schema, model } from 'mongoose';
 export interface ICarPlate {
     plate: string;
 };
+
+export const CarPlateSchema = new Schema<ICarPlate>({
+    plate: {type: String, required: true},
+});
+
+export const CarPlateModel = model<ICarPlate>("CarPlate", CarPlateSchema);
