de438c004cdd6e336ebcd325f4b0af06ccd7adcc
Eduardo Cortez Valente <130951926+EduardoCorVal@users.noreply.github.com>
2023-10-04 18:20:31 -0600

    feature(CarPlates): schema done

--- src/models/CarPlates.ts
+++ src/models/CarPlates.ts
@@ -1,11 +1,27 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, Types } from 'mongoose';
+
+export interface ITransaction {
+    timestamp: Date;
+    duration_minutes_transaction: number;
+};
+
+export const TransactionSchema = new Schema<ITransaction>({
+    timestamp: {type: Date, required: true, default: Date.now},
+    duration_minutes_transaction: {type: Number, required: false},
+});
 
 export interface ICarPlate {
+    branch_id: Types.ObjectId;
     plate: string;
+    username: string;
+    transactions?: Types.DocumentArray<ITransaction>
 };
 
 export const CarPlateSchema = new Schema<ICarPlate>({
+    branch_id : {type: Schema.Types.ObjectId, required: true, ref: 'Branch' },
     plate: {type: String, required: true},
+    username: {type: String, required: false},
+    transactions: {type: [TransactionSchema], default: [], required:false},
 });
 
 export const CarPlateModel = model<ICarPlate>("CarPlate", CarPlateSchema);
