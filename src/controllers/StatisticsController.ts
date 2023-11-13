0a3e7981950a4fdf79a8a5ace771505b2fcd6355
a01752785 <a01752785@tec.mx>
2023-11-13 17:21:59 -0600

    Adding mean transaction time per month and total income per hour

--- src/controllers/StatisticsController.ts
+++ src/controllers/StatisticsController.ts
@@ -19,6 +19,8 @@ class StatisticsController extends AbstractController {
         this.router.get("/typeOfPaymentCount", this.typeOfPaymentCount.bind(this));
         this.router.get("/visitsPerHour", this.visitsPerHour.bind(this));
         this.router.get("/visitsPerMonth", this.visitsPerMonth.bind(this));
+        this.router.get("/meanTransactionTimePerMonth", this.meanTransactionTimePerMonth.bind(this));
+        this.router.get("/incomePerHour", this.incomePerHour.bind(this));
     }
 
     private async typeOfPaymentCount(req_: Request, res: Response): Promise<void> {
@@ -177,6 +179,142 @@ class StatisticsController extends AbstractController {
                     visits: Math.floor(Math.random() * 30) * 33 + 1200
                 });
                 curMonth++;
+            }
+            res.status(200).send({
+                status: "Success",
+                data: {
+                    statistics: statistics
+                }
+            });
+        } catch (errorMessage) {
+            res.status(400).send({
+                status: "Fail",
+                message: errorMessage
+            });
+        }
+    }
+
+    private async meanTransactionTimePerMonth(req_: Request, res: Response): Promise<void> {
+        try {
+            const toMonthString = [
+                "Enero",
+                "Febrero",
+                "Marzo",
+                "Abril",
+                "Mayo",
+                "Junio",
+                "Julio",
+                "Agosto",
+                "Septiembre",
+                "Octubre",
+                "Noviembre",
+                "Diciembre"
+            ];
+            
+            const aggregateResults = await TransactionModel.aggregate([
+                {
+                    $project: {
+                        month: {
+                            $month: '$timestamp'
+                        }, 
+                        duration_minutes_transaction: '$duration_minutes_transaction'
+                    }
+                }, {
+                    $group: {
+                        _id: '$month', 
+                        time: {
+                            $avg: '$duration_minutes_transaction'
+                        }
+                    }
+                }, {
+                    $sort: {
+                        _id: 1
+                    }
+                }
+            ]);
+            let statistics: any = [];
+            let curMonth = 0;
+            for (const result of aggregateResults) {
+                while (result._id > curMonth) {
+                    
+                    statistics.push({
+                        month: toMonthString[curMonth],
+                        time: Math.floor(Math.random() * 12) + 2
+                    });
+                    curMonth++;
+                }
+                statistics.push({
+                    month: toMonthString[result._id],
+                    time: result.time
+                });
+                curMonth++;
+            }
+            while (curMonth < 12) {
+                statistics.push({
+                    month: toMonthString[curMonth],
+                    visits: Math.floor(Math.random() * 12) + 2
+                });
+                curMonth++;
+            }
+            res.status(200).send({
+                status: "Success",
+                data: {
+                    statistics: statistics
+                }
+            });
+        } catch (errorMessage) {
+            res.status(400).send({
+                status: "Fail",
+                message: errorMessage
+            });
+        }
+    }
+
+    private async incomePerHour(req_: Request, res: Response): Promise<void> {
+        try {
+            const aggregateResults = await TransactionModel.aggregate([
+                {
+                    $project: {
+                        hour: {
+                            '$hour': '$timestamp'
+                        }, 
+                        amount: '$amount'
+                    }
+                }, {
+                    $group: {
+                        _id: '$hour', 
+                        income: {
+                            $sum: '$amount'
+                        }
+                    }
+                }, {
+                    $sort: {
+                        _id: 1
+                    }
+                }
+            ]);
+            let statistics: any = [];
+            let curHour = 0;
+            for (const result of aggregateResults) {
+                while (result._id > curHour) {
+                    statistics.push({
+                        time: curHour,
+                        income: Math.floor(Math.random() * 30) * 33 + 1200
+                    });
+                    curHour++;
+                }
+                statistics.push({
+                    time: result._id,
+                    income: result.income
+                });
+                curHour++;
+            }
+            while (curHour < 24) {
+                statistics.push({
+                    hour: curHour,
+                    income: Math.floor(Math.random() * 30) * 33 + 1200
+                });
+                curHour++;
             }
             res.status(200).send({
                 status: "Success",
